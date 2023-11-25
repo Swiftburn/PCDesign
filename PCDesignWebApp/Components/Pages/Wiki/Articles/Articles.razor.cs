2646f4278ca9ab9b9f3ee650254bcec815464f97
Swiftburn <113044118+Swiftburn@users.noreply.github.com>
2023-11-25 13:02:57 -0600

    Added & Merged Previous Repo
    Due to how different the setups are, the wiki is now broken and requires fixing.

---
+++ PCDesignWebApp/Components/Pages/Wiki/Articles/Articles.razor.cs
@@ -0,0 +1,7 @@
+// public partial class Articles
+// {
+//     public int Id { get; set; }
+//     public string Title { get; set; }
+//     public string Content { get; set; }
+//     public DateTime LastModified { get; set; }
+// }

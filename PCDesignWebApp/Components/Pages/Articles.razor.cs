0a855252322099326f792a7050bb3919f3f8305b
Swiftburn <113044118+Swiftburn@users.noreply.github.com>
2023-11-25 23:03:12 -0600

    Updated Project Structure Files
    Updated files so that they display correctly when ran

--- PCDesignWebApp/Components/Pages/Articles.razor.cs
+++ PCDesignWebApp/Components/Pages/Articles.razor.cs
@@ -1,7 +1,18 @@
-// public partial class Articles
-// {
-//     public int Id { get; set; }
-//     public string Title { get; set; }
-//     public string Content { get; set; }
-//     public DateTime LastModified { get; set; }
-// }
+/**
+ * @file Articles.razor.cs
+ * @author Adolfo Barrientos (badolfo4@gmail.com)
+ * @brief This file provides the c# for the Articles portion of the web application. 
+ * @version 0.21
+ * @date 2023-11-11
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+public partial class Articles
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime LastModified { get; set; }
+}

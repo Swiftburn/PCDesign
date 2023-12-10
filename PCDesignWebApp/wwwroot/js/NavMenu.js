5ba200a865b62b186ce64ef93e473f143a520729
thomasdang92 <thomasdang92@gmail.com>
2023-12-10 07:08:13 -0600

    Changed organization of NavMenu JS.

---
+++ PCDesignWebApp/wwwroot/js/NavMenu.js
@@ -0,0 +1,36 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    document.body.addEventListener('click', function (event) {
+        if (event.target.closest('#navbarCollapse')) {
+            updateLoginAccountLink();
+        }
+    });
+
+    document.addEventListener('userLogin', updateLoginAccountLink);
+    document.addEventListener('userLogout', updateLoginAccountLink);
+    updateLoginAccountLink();
+});
+
+window.updateLoginAccountLink = function () {
+    let userCreds = JSON.parse(sessionStorage.getItem("user-creds"));
+    let loginAccountReturn = document.getElementById('loginAccountReturn');
+
+    if (userCreds !== null) {
+        loginAccountReturn.innerHTML = (
+            '<a class="nav-link" href="Account">' +
+            '<div>' +
+            '<div class="nav-link-icon-padding"><span class="oi oi-person" aria-hidden="true"></span></div>' +
+            '<div>Account</div>' +
+            '</div>' +
+            '</a>'
+        );
+    } else {
+        loginAccountReturn.innerHTML = (
+            '<a class="nav-link" href="Login">' +
+            '<div>' +
+            '<div class="nav-link-icon-padding"><span class="oi oi-person" aria-hidden="true"></span></div>' +
+            '<div>Login</div>' +
+            '</div>' +
+            '</a>'
+        );
+    }
+};

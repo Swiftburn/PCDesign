43c93fae9d43724ce2d8c48555a9364aa2c099a3
thomasdang92 <thomasdang92@gmail.com>
2023-12-11 20:15:01 -0600

    Moved Account JS and Added Sign Out Functionality

---
+++ PCDesignWebApp/wwwroot/js/Account.js
@@ -0,0 +1,49 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    function loadUserData() {
+        return new Promise((resolve) => {
+            let UserCreds = JSON.parse(sessionStorage.getItem("user-creds"));
+            let UserInfo = JSON.parse(sessionStorage.getItem("user-info"));
+
+            if (UserCreds && UserInfo) {
+                resolve({ UserCreds, UserInfo });
+            } else {
+                setTimeout(() => loadUserData().then(resolve), 100);
+            }
+        });
+    }
+
+    function updateProfile() {
+        loadUserData().then(({ UserCreds, UserInfo }) => {
+            let EmailHead = document.getElementById('emaildiv');
+            EmailHead.innerText = `Email: ${UserCreds.email}`;
+
+            let FirstNameHead = document.getElementById('firstnamediv');
+            FirstNameHead.innerText = `First Name: ${UserInfo.firstName}`;
+
+            let LastNameHead = document.getElementById('lastnamediv');
+            LastNameHead.innerText = `Last Name: ${UserInfo.lastName}`;
+        });
+    }
+
+    // Initial update
+    updateProfile();
+
+    // Update on hash change
+    window.onhashchange = updateProfile;
+
+    function loadSavedBuilds() {
+
+    }
+
+    loadSavedBuilds();
+
+    let SignoutBtn = document.getElementById('signoutButton');
+
+    let Signout = () => {
+        location.href = "/login";
+        sessionStorage.removeItem("user-creds");
+        sessionStroage.removeItem("user-info");
+    }
+
+    SignoutBtn.addEventListener('click', Signout);
+});
